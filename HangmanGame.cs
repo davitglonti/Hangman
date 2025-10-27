fd33b684f2d6262acdca4581f11998ee4354e4dd
davitgl <dglonti3@gmail.com>
2025-10-27 21:03:21 +0400

    Update HangGame project

---
+++ HangmanGame.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangGame
+{
+    public class HangmanGame
+    {
+        private List<string> words;
+        private string wordToGuess;
+        private char[] guessedLetters;
+        private HashSet<char> triedLetters;
+        private int maxWrongGuesses;
+        private int wrongGuesses;
+
+        public HangmanGame(List<string> words, int maxWrongGuesses = 6)
+        {
+            this.words = words;
+            this.maxWrongGuesses = maxWrongGuesses;
+            this.triedLetters = new HashSet<char>();
+
+
+        }
+
+        public void StartNewGame()
+        {
+            Random rand = new Random();
+            wordToGuess = words[rand.Next(words.Count)].ToUpper();
+            guessedLetters = new string('_', wordToGuess.Length).ToCharArray();
+            wrongGuesses = 0;
+            triedLetters.Clear();
+        }
+
+        // test method to get the length of the word to guess
+        public int GetWordLength() => wordToGuess.Length;
+
+
+        public bool GuessLetter(char letter)
+        {
+            letter = char.ToUpper(letter);
+
+            if (triedLetters.Contains(letter))
+            {
+                Console.WriteLine($"You already tried '{letter}'!");
+                return false;
+            }
+
+            triedLetters.Add(letter);
+            bool found = false;
+
+            for (int i = 0; i < wordToGuess.Length; i++)
+            {
+                if (wordToGuess[i] == letter)
+                {
+                    guessedLetters[i] = letter;
+                    found = true;
+                }
+            }
+
+            if (!found)
+            {
+                wrongGuesses++;
+                Console.WriteLine($"Wrong guess! You have {maxWrongGuesses - wrongGuesses} tries left.");
+            }
+
+            return found;
+        }
+        public string GetCurrentWordState()
+        {
+            return string.Join(" ", guessedLetters);
+        }
+    }
+
+}
+
+
+     
+    
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HangGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = new List<string> { "APPLE", "BANANA", "ORANGE", "MANGO" };
+            HangmanGame game = new HangmanGame(words, 6);
+
+            game.StartNewGame();
+
+            Console.WriteLine("Game started");
+            Console.WriteLine("New game word length: " + game.GetWordLength());
+
+            Console.WriteLine("\nGuessing A:");
+            game.GuessLetter('A');
+            Console.WriteLine("Current word: " + game.GetCurrentWordState());
+
+            Console.WriteLine("\nGuessing Z:");
+            game.GuessLetter('Z');
+            Console.WriteLine("Current word: " + game.GetCurrentWordState());
+
+        }
+    }
+}
